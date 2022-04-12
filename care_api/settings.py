f533776443f4a20bf8ab2d9450630e4f6e927348
Shiyang Wang <shiyangwang@umass.edu>
2022-04-12 15:13:09 -0400

    cors allowall

--- care_api/settings.py
+++ care_api/settings.py
@@ -190,15 +190,16 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://nelab.ddns.umass.edu",
-    "https://nelab.ddns.umass.edu",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3001",
+#     "http://127.0.0.1:3001",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://nelab.ddns.umass.edu",
+#     "https://nelab.ddns.umass.edu",
+# ]
 
 
 
