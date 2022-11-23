7bad584c67591ebb3f1c6df69359e141f4474e7e
Beizong Chen <beizongchen@umass.edu>
2022-11-23 22:58:59 +0000

    Added Beizong Chen's Email as code sender email.

--- care_api/settings.py
+++ care_api/settings.py
@@ -217,4 +217,10 @@ PASSWORDLESS_AUTH = {
     'PASSWORDLESS_EMAIL_NOREPLY_ADDRESS': 'noreply@example.com',
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'beizongchen@umass.edu'
+EMAIL_HOST_PASSWORD = 'kjvqerhxeutpbtxa'
